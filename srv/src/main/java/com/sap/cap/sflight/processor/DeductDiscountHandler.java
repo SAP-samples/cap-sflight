681a14db158281793d8bef25b1bcf5282ba2b20d
Robin de Silva Jayasinghe <r.de.silva.jayasinghe@sap.com>
2021-05-31 08:11:44 +0200

    deduct discount rounding fix
    now also works in active and draft mode

--- srv/src/main/java/com/sap/cap/sflight/processor/DeductDiscountHandler.java
+++ srv/src/main/java/com/sap/cap/sflight/processor/DeductDiscountHandler.java
@@ -2,21 +2,20 @@ package com.sap.cap.sflight.processor;
 
 import static cds.gen.travelservice.TravelService_.TRAVEL;
 
-import java.math.BigDecimal;
-import java.util.Map;
-
+import cds.gen.travelservice.DeductDiscountContext;
+import cds.gen.travelservice.Travel;
+import cds.gen.travelservice.TravelService_;
+import cds.gen.travelservice.Travel_;
 import com.sap.cds.ql.Update;
 import com.sap.cds.services.draft.DraftService;
 import com.sap.cds.services.handler.EventHandler;
 import com.sap.cds.services.handler.annotations.On;
 import com.sap.cds.services.handler.annotations.ServiceName;
-
 import org.springframework.stereotype.Component;
 
-import cds.gen.travelservice.DeductDiscountContext;
-import cds.gen.travelservice.Travel;
-import cds.gen.travelservice.TravelService_;
-import cds.gen.travelservice.Travel_;
+import java.math.BigDecimal;
+import java.math.MathContext;
+import java.util.Map;
 
 @Component
 @ServiceName(TravelService_.CDS_NAME)
@@ -31,11 +30,13 @@ public class DeductDiscountHandler implements EventHandler {
     @On(event = "deductDiscount", entity = Travel_.CDS_NAME)
     public void deductDiscount(final DeductDiscountContext context) {
 
-        Travel travel = db.run(context.getCqn()).single(Travel.class);
-        BigDecimal discount = BigDecimal.valueOf(context.getPercent()).divide(BigDecimal.valueOf(100));
+        var travel = db.run(context.getCqn()).single(Travel.class);
+        BigDecimal discount = BigDecimal.valueOf(context.getPercent()).divide(BigDecimal.valueOf(100),
+                new MathContext(3));
 
-        BigDecimal deductedTotalPrice = travel.getTotalPrice().subtract(travel.getBookingFee().multiply(discount));
-        BigDecimal deductedBookingFee = travel.getBookingFee().subtract(travel.getBookingFee().multiply(discount));
+        BigDecimal deductedBookingFee = travel.getBookingFee().subtract(travel.getBookingFee().
+                multiply(discount)).round(new MathContext(3));
+        BigDecimal deductedTotalPrice = travel.getTotalPrice().subtract(deductedBookingFee);
 
         travel.setTotalPrice(deductedTotalPrice);
         travel.setBookingFee(deductedBookingFee);
@@ -44,7 +45,7 @@ public class DeductDiscountHandler implements EventHandler {
                 .data(Map.of("BookingFee", deductedBookingFee, "TotalPrice", deductedTotalPrice, "TravelUUID",
                         travel.getTravelUUID(), "IsActiveEntity", travel.getIsActiveEntity()));
 
-        if (travel.getIsActiveEntity()) {
+        if (Boolean.TRUE.equals(travel.getIsActiveEntity())) {
             db.run(updateCqn);
         } else {
             db.patchDraft(updateCqn);
