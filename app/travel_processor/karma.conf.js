5e81ca59e8f2d3b8a14441476b9b7729a70073a5
Dirk Teufel <50441133+drktfl@users.noreply.github.com>
2021-10-20 11:52:23 +0200

    Add karma server launcher for java

--- app/travel_processor/karma.conf.js
+++ app/travel_processor/karma.conf.js
@@ -1,112 +1,60 @@
-/*eslint no-unused-vars: 0*/
-function ODataServicesFactory(config, logger) {
-  const { exec } = require("child_process"),
-    { createProxyMiddleware } = require("http-proxy-middleware"),
-    log = logger.create("odata-services"),
-    options = config.cap,
-    port = options.port || 4004,
-    cwd = options.root || __dirname;
+const puppeteer = require("puppeteer"),
+    capServer = require("./karma/cap-server");
 
-  const server = new Promise((resolve, reject) => {
-    const proc = exec("npx cds run", {
-      cwd: cwd,
-      env: { ...process.env, PORT: port },
-    });
+process.env.CHROME_BIN = puppeteer.executablePath();
 
-    proc.on("error", (err) => {
-      reject(err);
-    });
+module.exports = async config => {
+    // start the CAP server (either specify CLI arg --server=node or --server=java)
+    const cap = await capServer[config.server]()
 
-    proc.stdout.on("data", (data) => {
-      data = data.toString().trim();
-      log.info(data);
-      if (data.includes("server listening on")) {
-        log.info("Server ready");
-        resolve(proc);
-      }
-    });
-
-    proc.stderr.on("data", (data) => {
-      log.error(data.toString().trim());
-    });
-
-    proc.on("close", (code, signal) => {
-      log.info(`CDS server exited with code ${code} by signal ${signal}`);
-      if (code) reject(code);
-    });
-  });
-
-  return createProxyMiddleware("!/base/**", {
-    target: `http://localhost:${port}`,
-    async pathRewrite(path) {
-      await server;
-      return path;
-    },
-    logProvider(provider) {
-      return log;
-    },
-  });
-}
-
-ODataServicesFactory.$inject = ["config", "logger"];
-
-process.env.CHROME_BIN = require("puppeteer").executablePath();
-
-module.exports = function (config) {
-  const port = process.env.PORT || 4004;
-
-  config.set({
-    frameworks: ["ui5", "qunit"],
-    logLevel: "ERROR", // log errors only. Change to "DEBUG" for more verbosity
-    proxies: {
-      "/base/webapp/processor": "/processor",
-    },
-    client: {
-      qunit: {
-        showUI: true,
-        testTimeout: 100000,
-      },
-    },
-    ui5: {
-      url: "https://ui5.sap.com",
-      mode: "script",
-      config: {
-        bindingSyntax: "complex",
-        language: "en",
-        async: true,
-        animation: false,
-        libs: "sap.fe.core,sap.ui.core,sap.fe.test",
-        compatVersion: "edge",
-        theme: "sap_fiori_3",
-        logLevel: "ERROR",
-        resourceRoots: {
-          "sap.fe.cap.travel": "./base/webapp",
-          test: "./base/webapp/test/",
+    config.set({
+        frameworks: ["ui5", "qunit"],
+        logLevel: "ERROR", // log errors only. Change to "DEBUG" for more verbosity
+        proxies: {
+            "/base/webapp/processor": "/processor",
         },
-      },
-      tests: ["sap/fe/cap/travel/test/integration/Opa.qunit"],
-    },
-    plugins: [
-      ...config.plugins,
-      { "middleware:odata-services": ["factory", ODataServicesFactory] },
-    ],
-    middleware: ["odata-services"],
-    reporters: ["mocha"],
-    cap: {
-      port: port,
-      root: "../..",
-    },
-    browsers: ["CustomChromeHeadless"],
-    customLaunchers: {
-      CustomChrome: {
-        base: "Chrome",
-        flags: ["--window-size=1280,1024", "--no-sandbox"],
-      },
-      CustomChromeHeadless: {
-        base: "ChromeHeadless",
-        flags: ["--window-size=1280,1024", "--no-sandbox"],
-      },
-    },
-    singleRun: true,
-  });
+        client: {
+            qunit: {
+                showUI: true,
+                testTimeout: 100000,
+            },
+        },
+        ui5: {
+            url: "https://ui5.sap.com",
+            mode: "script",
+            config: {
+                bindingSyntax: "complex",
+                language: "en",
+                async: true,
+                animation: false,
+                libs: "sap.fe.core,sap.ui.core,sap.fe.test",
+                compatVersion: "edge",
+                theme: "sap_fiori_3",
+                logLevel: "ERROR",
+                resourceRoots: {
+                    "sap.fe.cap.travel": "./base/webapp",
+                    "test": "./base/webapp/test/",
+                },
+            },
+            tests: ["sap/fe/cap/travel/test/integration/Opa.qunit"],
+        },
+        plugins: [
+            ...config.plugins,
+            cap
+        ],
+        middleware: ["cap-proxy"],
+        reporters: ["mocha"],
+        browsers: config.ci ? ["CustomChromeHeadless"] : ["CustomChrome"],
+        customLaunchers: {
+            CustomChrome: {
+                base: "Chrome",
+                flags: ["--window-size=1280,1024", "--no-sandbox"],
+            },
+            CustomChromeHeadless: {
+                base: "ChromeHeadless",
+                flags: ["--window-size=1280,1024", "--no-sandbox"],
+            },
+        },
+        singleRun: true
+    });
 };
---
+++ app/travel_processor/karma/cap-server.js
@@ -0,0 +1,64 @@
+const spawn = require("cross-spawn"), HttpProxy = require("http-proxy");
+
+function spawnServer(cmd, args, cwd, fnIsReady) {
+    return new Promise((resolve, reject) => {
+        const proc = spawn(cmd, args, {cwd, stdio: ["ignore", "pipe", "inherit"]});
+
+        const checkServerReady = (data) => {
+            const targetUrl = fnIsReady(data.toString())
+            if (targetUrl) {
+                proc.stdout.removeListener("data", checkServerReady)
+                resolve(targetUrl);
+            }
+        }
+
+        proc.on("close", reject);
+        proc.stdout.on("data", checkServerReady);
+        proc.stdout.pipe(process.stdout);
+    });
+}
+
+function createKarmaMiddleware(serverUrl, auth) {
+    const proxyOptions = {
+        target: serverUrl,
+        auth: auth ? `${auth.user}:${auth.password}` : undefined
+    }
+
+    const middleware = (logFactory) => {
+        const log = logFactory.create("cap-server");
+
+        const proxy = new HttpProxy(proxyOptions);
+        proxy.on("error", data => log.error(data.toString()));
+
+        return (req, res) => {
+            proxy.web(req, res)
+        }
+    }
+
+    middleware.$inject = ["logger"];
+    return {"middleware:cap-proxy": ["factory", middleware]}
+}
+
+
+async function java() {
+    const isReady = (data) => {
+        const port = data.match(/started on port\(s\): (?<port>\d+)/)?.groups?.port
+        if (port) return new URL(`http://localhost:${port}`)
+    }
+    const serverUrl = await spawnServer("mvn", ["spring-boot:run", "-Dserver.port=0"], "../../srv", isReady)
+
+    return createKarmaMiddleware(serverUrl, {user: "admin", password: "admin"})
+}
+
+async function node() {
+    const isReady = (data) => {
+        const url = data.match(/server listening on {.*url:.*'(?<url>.+)'.*}/)?.groups?.url
+        if (url) return new URL(url)
+    }
+    const serverUrl = await spawnServer("npm", ["start"], "../..", isReady)
+
+    return createKarmaMiddleware(serverUrl, {user: "admin", password: "admin"})
+}
+
+
+module.exports = {java, node}
