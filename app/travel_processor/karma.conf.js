e18ed1c10c0172a3000ef6a9ea310b2909a3214a
Dirk Teufel <50441133+drktfl@users.noreply.github.com>
2022-02-01 15:15:20 +0100

    Set Karma single-run option in CI
    Signed-off-by: Dirk Teufel <50441133+drktfl@users.noreply.github.com>

--- app/travel_processor/karma.conf.js
+++ app/travel_processor/karma.conf.js
@@ -26,5 +26,6 @@ module.exports = async (config) => {
     plugins: [...config.plugins, await capMiddleware],
     middleware: ["cap-proxy"],
     browsers: config.ci ? ["ChromeHeadless"] : ["Chrome"],
+    singleRun: config.ci ?? config.singleRun ?? false
   });
 };
