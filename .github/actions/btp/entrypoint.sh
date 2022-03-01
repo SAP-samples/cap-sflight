fdb2130f8714703b9f7f25495295300d38a1054e
Christian Georgi <christian.georgi@sap.com>
2022-03-01 11:34:47 +0100

    Assign extra users

--- .github/actions/btp/entrypoint.sh
+++ .github/actions/btp/entrypoint.sh
@@ -9,6 +9,18 @@ if [ ! -z "${INPUT_SUBACCOUNT_ID}" ]; then
   btp target --subaccount "${INPUT_SUBACCOUNT_ID}"
 fi
 
+if [ ! -z "${INPUT_ROLE_COLLECTION}" ]; then
+  btp assign security/role-collection ${INPUT_ROLE_COLLECTION} --to-user ${INPUT_USERNAME} > /dev/null
+
+  if [ ! -z "${GRANT_USERS}" ]; then
+    for user in ${GRANT_USERS//\\n/ }  # newline separated
+    do
+      btp assign security/role-collection ${INPUT_ROLE_COLLECTION} --to-user ${user} --create-user-if-missing
+    done
+  fi
+
+fi
+
 if [ ! -z "${INPUT_COMMAND}" ]; then
   ${INPUT_COMMAND}
 fi
