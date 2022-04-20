497478a9a58f7a36f135fb34093b6ce8d1e154fe
Mikhail Goncharov <22234117+MikhailGoncharov@users.noreply.github.com>
2022-04-20 13:39:22 +0200

    remove workaround for IsActiveEntity as string for cds v6.0.0

--- srv/workarounds.js
+++ srv/workarounds.js
@@ -55,7 +55,11 @@ cds.extend (cds.Request) .with (class {
       // deviate to draft?
       const {IsActiveEntity} = key
       if (IsActiveEntity !== undefined) Object.defineProperty (key,'IsActiveEntity',{value:IsActiveEntity, enumerable:false}) //> skip as key in cqn
-      if (IsActiveEntity === 'false') target = target.drafts // REVISIT: Why is IsActiveEntity a string, and not a boolean?
+      if (cds.version < '6.0.0') {
+        if (IsActiveEntity === 'false') target = target.drafts
+      } else {
+        if (IsActiveEntity === false) target = target.drafts
+      }
       // prepare target query
       const q = SELECT.one.from(target,key)
       const {from:{ref},where} = q.SELECT
