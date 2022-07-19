df76638a1471296966382c0b6c482c4c562edf2f
Daniel Schlachter <schlachter.daniel@gmail.com>
2022-07-19 11:18:04 +0200

    handlers for cancel supplement and booking to update totals, consider side effects (#213)
    * handlers for cancel supplement and booking to update totals, handle side effect of delete
    * Update srv/travel-service.js
    
    Co-authored-by: Dr. David A. Kunz <david.kunz@sap.com>
    Co-authored-by: Dr. David A. Kunz <david.kunz@sap.com>
    Co-authored-by: Robin <r.de.silva.jayasinghe@sap.com>

--- srv/travel-service.js
+++ srv/travel-service.js
@@ -83,6 +83,38 @@ init() {
     return this._update_totals4 (travel)
   }})
 
+  /**
+   * Update the Travel's TotalPrice when a Booking Supplement is deleted.
+   */
+  this.on('CANCEL', BookingSupplement, async (req, next) => {
+    // Find out which travel is affected before the delete
+    const { DraftAdministrativeData_DraftUUID, BookSupplUUID } = req.data
+    const { to_Travel_TravelUUID } = await SELECT.one
+      .from(BookingSupplement.drafts, ['to_Travel_TravelUUID'])
+      .where({ DraftAdministrativeData_DraftUUID, BookSupplUUID })
+    // Delete handled by generic handlers
+    const res = await next()
+    // After the delete, update the totals
+    await this._update_totals4(to_Travel_TravelUUID)
+    return res
+  })
+  
+  /**
+   * Update the Travel's TotalPrice when a Booking is deleted.
+   */
+  this.on('CANCEL', Booking, async (req, next) => {
+    // Find out which travel is affected before the delete
+    const { DraftAdministrativeData_DraftUUID, BookingUUID } = req.data
+    const { to_Travel_TravelUUID } = await SELECT.one
+      .from(Booking.drafts, ['to_Travel_TravelUUID'])
+      .where({ DraftAdministrativeData_DraftUUID, BookingUUID })
+    // Delete handled by generic handlers
+    const res = await next()
+    // After the delete, update the totals
+    await this._update_totals4(to_Travel_TravelUUID)
+    return res
+  })
+
 
   /**
    * Helper to re-calculate a Travel's TotalPrice from BookingFees, FlightPrices and Supplement Prices.
