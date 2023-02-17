f5a9ee2363310b975f2c642c751d0eb4ad5c0f71
Dirk Teufel <50441133+drktfl@users.noreply.github.com>
2023-02-17 16:27:13 +0100

    Update the apps (#445)

--- app/travel_processor/webapp/test/testsuite.qunit.js
+++ app/travel_processor/webapp/test/testsuite.qunit.js
@@ -2,8 +2,11 @@
 window.suite = function () {
   "use strict";
 
-  let oSuite = new parent.jsUnitTestSuite();
-  let sContextPath = location.pathname.substring(0, location.pathname.lastIndexOf("/"));
+  const oSuite = new parent.jsUnitTestSuite();
+  const sContextPath = location.pathname.substring(
+    0,
+    location.pathname.lastIndexOf("/")
+  );
 
   oSuite.addTestPage(`${sContextPath}/integration/Opa.qunit.html`);
 
