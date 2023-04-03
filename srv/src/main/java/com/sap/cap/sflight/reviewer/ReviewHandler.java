ca9c370ddf3bbf31e92a50db020796d81627a365
Robin de Silva Jayasinghe <r.de.silva.jayasinghe@sap.com>
2023-04-03 07:55:18 +0200

    read actual review data for event

--- srv/src/main/java/com/sap/cap/sflight/reviewer/ReviewHandler.java
+++ srv/src/main/java/com/sap/cap/sflight/reviewer/ReviewHandler.java
@@ -5,6 +5,9 @@ import java.math.BigDecimal;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
+import com.sap.cds.Result;
+import com.sap.cds.Row;
+import com.sap.cds.ql.Select;
 import com.sap.cds.services.cds.CqnService;
 import com.sap.cds.services.handler.EventHandler;
 import com.sap.cds.services.handler.annotations.After;
@@ -13,13 +16,14 @@ import com.sap.cds.services.handler.annotations.ServiceName;
 import cds.gen.reviewservice.ReviewService_;
 import cds.gen.reviewservice.Reviewed;
 import cds.gen.reviewservice.ReviewedContext;
+import cds.gen.reviewservice.TravelReview;
 import cds.gen.reviewservice.TravelReview_;
+import cds.gen.travelservice.TravelService_;
 
 @Component
 @ServiceName(ReviewService_.CDS_NAME)
 public class ReviewHandler implements EventHandler {
 
-	
 	private final CqnService reviewService;
 
 	public ReviewHandler(@Qualifier(ReviewService_.CDS_NAME) CqnService reviewService) {
@@ -28,18 +32,23 @@ public class ReviewHandler implements EventHandler {
 
 	@After(event = { CqnService.EVENT_CREATE, CqnService.EVENT_UPDATE, CqnService.EVENT_UPSERT,
 			CqnService.EVENT_DELETE }, entity = TravelReview_.CDS_NAME)
-	public void afterReviewWrite() {
+	public void afterReviewWrite(TravelReview travelReview) {
 
-		//TODO: read the actual review data
+		Select<TravelReview_> averageRatingQuery = Select.from(ReviewService_.TRAVEL_REVIEW)
+				.columns(t -> t.Rating().average().as("average"), t -> t.Rating().countDistinct().as("reviewCount"))
+				.where(t -> t.TravelID().eq(travelReview.getTravelID()));
+
+		Row resultRow = reviewService.run(averageRatingQuery).single();
+		
 
 		Reviewed reviewed = Reviewed.create();
-		reviewed.setCount(1);
+		reviewed.setCount(Integer.valueOf(resultRow.get("reviewCount").toString()));
 		reviewed.setRating(BigDecimal.valueOf(5L));
-		reviewed.setSubject(4132);
+		reviewed.setSubject(travelReview.getTravelID());
 
 		ReviewedContext reviewedContext = ReviewedContext.create();
 		reviewedContext.setData(reviewed);
-	
+
 		reviewService.emit(reviewedContext);
 	}
 }
