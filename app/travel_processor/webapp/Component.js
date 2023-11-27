305a1883ab468af60e0742583d72f7caa6af202c
Daniel Hutzel <daniel.hutzel@sap.com>
2023-11-27 14:03:35 +0100

    Cleanup + golden path

--- app/travel_processor/webapp/Component.js
+++ app/travel_processor/webapp/Component.js
@@ -1,6 +1,6 @@
-sap.ui.define(["sap/fe/core/AppComponent"], function (AppComponent) {
+sap.ui.define(["sap/fe/core/AppComponent"], function (Component) {
   "use strict";
-  return AppComponent.extend("sap.fe.cap.travel.Component", {
+  return Component.extend("sap.fe.cap.travel.Component", {
     metadata: { manifest: "json" },
   });
 });
