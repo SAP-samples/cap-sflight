9f60956ceee1b4b915b3a890374653401caaccd4
Dr. David A. Kunz <david.kunz@sap.com>
2024-05-28 14:47:35 +0200

    fix content type

--- test/odata.test.js
+++ test/odata.test.js
@@ -43,12 +43,9 @@ describe ("Basic Querying", () => {
 describe('Basic OData', () => {
 
   it('serves $metadata documents in v4', async () => {
-    const { headers, status, data } = await GET `/processor/$metadata`
+    const { headers, status, data } = await GET(`/processor/$metadata`, { headers: { Accept: 'application/xml' } } )
     expect(status).to.equal(200)
-    expect(headers).to.contain({
-      'content-type': 'application/xml',
-      'odata-version': '4.0',
-    })
+    expect(headers['content-type']).to.match(/application\/xml/)
     expect(data).to.contain('<EntitySet Name="Travel" EntityType="TravelService.Travel">')
     expect(data).to.contain('<Annotation Term="Common.Label" String="Travel"/>')
   })
