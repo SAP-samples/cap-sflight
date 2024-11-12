134a2e0b75aeda1a051bb8d57f1356ca0e7106e1
Kolbowski, Christian <christian.kolbowski@sap.com>
2024-11-12 15:26:14 +0100

    ts config continued

--- app/travel_processor/webapp/Component.js
+++
@@ -1,6 +0,0 @@
-sap.ui.define(["sap/fe/core/AppComponent"], function (AppComponent) {
-  "use strict";
-  return AppComponent.extend("sap.fe.cap.travel.Component", {
-    metadata: { manifest: "json" },
-  });
-});
---
+++ app/travel_processor/webapp/Component.ts
@@ -0,0 +1,20 @@
+import BaseComponent from "sap/fe/core/AppComponent";
+
+/**
+ * @namespace sap.fe.cap.managetravels
+ */
+export default class Component extends BaseComponent {
+
+	public static metadata = {
+		manifest: "json"
+	};
+
+    /**
+     * The component is initialized by UI5 automatically during the startup of the app and calls the init method once.
+     * @public
+     * @override
+     */
+	//public init() : void {
+    //    super.init();
+	//}
+}
