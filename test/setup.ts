d7dc54dd3030edf54a91b7780e97605a554baeaa
Christian Georgi <chgeo@users.noreply.github.com>
2024-12-03 13:03:17 +0100

    Add comment

--- test/setup.ts
+++ test/setup.ts
@@ -1,5 +1,6 @@
 // for mocha
 process.env.CDS_TYPESCRIPT = "true"
+// disables cds-ui5-plugin as otherwise it would not let mocha tests terminate
 process.env.CDS_PLUGIN_UI5_ACTIVE="false"
 
 // for jest
