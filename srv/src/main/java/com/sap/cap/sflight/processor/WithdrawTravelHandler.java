1bb3f51d4fda954107f29da84850ceebfdafa923
Jennifer Stamm <j.stamm@sap.com>
2025-05-07 15:16:50 +0200

    beaufiul new world

--- srv/src/main/java/com/sap/cap/sflight/processor/WithdrawTravelHandler.java
+++ srv/src/main/java/com/sap/cap/sflight/processor/WithdrawTravelHandler.java
@@ -1,9 +1,5 @@
 package com.sap.cap.sflight.processor;
 
-import static cds.gen.travelservice.TravelService_.TRAVEL;
-
-import cds.gen.analyticsservice.AnalyticsService;
-import cds.gen.analyticsservice.AnalyticsService.Draft;
 import cds.gen.travelservice.Travel;
 import cds.gen.travelservice.TravelService_;
 import cds.gen.travelservice.TravelWithdrawTravelContext;
@@ -21,6 +17,7 @@ import com.sap.cds.services.handler.annotations.Before;
 import com.sap.cds.services.handler.annotations.On;
 import com.sap.cds.services.handler.annotations.ServiceName;
 import com.sap.cds.services.persistence.PersistenceService;
+import com.sap.cds.services.utils.DraftUtils;
 import java.time.LocalDate;
 import org.springframework.stereotype.Component;
 
@@ -37,40 +34,30 @@ public class WithdrawTravelHandler implements EventHandler {
     }
 
     @Before(entity = Travel_.CDS_NAME)
-    public void check24HoursBeforeTravel(final TravelWithdrawTravelContext context) {
-        // TODO
-//        if (travel.beginDate().isBefore(LocalDate.now().minusDays(1))) {
-//            context.getMessages().error("Travel can only be withdrawn up to 24 hours before travel begins.");
-//            return;
-//        }
+    public void check24HoursBeforeTravel(final TravelWithdrawTravelContext context, CqnStructuredTypeRef travelRef) {
+        Travel travel = ((ApplicationService) context.getService()).run(
+                Select.from(travelRef).columns(Travel_.BEGIN_DATE)).first(Travel.class)
+            .orElseThrow(() -> new ServiceException(ErrorStatuses.BAD_REQUEST, "TRAVEL_NOT_FOUND"));
+
+        if (travel.beginDate().isBefore(LocalDate.now().minusDays(1))) {
+            context.getMessages().error("Travel can only be withdrawn up to 24 hours before travel begins.");
+        }
     }
 
     @On(entity = Travel_.CDS_NAME)
     public void onWithdrawTravel(final TravelWithdrawTravelContext context, CqnStructuredTypeRef travelRef) {
-        // TODO: ask whether context.cqn() or Select.from(ref) is preferred
-        // TODO specify columns
-        Travel travel = ((ApplicationService) context.getService()).run(Select.from(travelRef).columns(Travel_.BEGIN_DATE)).first(Travel.class)
-            .orElseThrow( () -> new ServiceException(ErrorStatuses.BAD_REQUEST, "TRAVEL_NOT_FOUND"));
-
-        // todo: moved
-        if (travel.beginDate().isBefore(LocalDate.now().minusDays(1))) {
-            context.getMessages().error("Travel can only be withdrawn up to 24 hours before travel begins.");
-            return;
-        }
-
-        // TODO: ask why travel.ref().asRef() fails a test but this doesn't -> noone knows
-        updateStatusForTravelId(context, travelRef, travel.isActiveEntity());
+        updateStatusForTravelId(context, travelRef);
         context.setCompleted();
     }
 
-    private void updateStatusForTravelId(TravelWithdrawTravelContext context, CqnStructuredTypeRef travelRef, boolean isActiveEntity) {
-        if (isActiveEntity) {
-            persistenceService.run(Update.entity(travelRef)
-                .data(Travel.TRAVEL_STATUS_CODE, TRAVEL_STATUS_WITHDRAWN));
+    private void updateStatusForTravelId(TravelWithdrawTravelContext context, CqnStructuredTypeRef travelRef) {
+        var isDraft = DraftUtils.isDraftTarget(travelRef,
+            context.getModel().findEntity(travelRef.targetSegment().id()).get(), context.getModel());
+        var update = Update.entity(travelRef).data(Travel.TRAVEL_STATUS_CODE, TRAVEL_STATUS_WITHDRAWN);
+        if (isDraft) {
+            ((DraftService) context.getService()).patchDraft(update).first(Travel.class);
         } else {
-            CqnUpdate travelUpdateDraft = Update.entity(travelRef)
-                .data(Travel.TRAVEL_STATUS_CODE, TRAVEL_STATUS_WITHDRAWN);
-            ((DraftService) context.getService()).patchDraft(travelUpdateDraft).first(Travel.class);
+            persistenceService.run(update);
         }
     }
 }
--- srv/src/test/java/com/sap/cap/sflight/processor/WithdrawTravelHandlerTest.java
+++ srv/src/test/java/com/sap/cap/sflight/processor/WithdrawTravelHandlerTest.java
@@ -1,22 +1,29 @@
 package com.sap.cap.sflight.processor;
 
+import static java.lang.Boolean.FALSE;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 import cds.gen.travelservice.Travel;
 import cds.gen.travelservice.TravelService;
+import cds.gen.travelservice.TravelWithdrawTravelContext;
 import cds.gen.travelservice.Travel_;
-import com.sap.cds.Result;
+import com.sap.cds.ql.CQL;
 import com.sap.cds.ql.Insert;
 import com.sap.cds.ql.Select;
-import com.sap.cds.ql.StructuredType;
+import com.sap.cds.ql.cqn.CqnAnalyzer;
 import com.sap.cds.ql.cqn.CqnSelect;
+import com.sap.cds.ql.cqn.CqnStatement;
+import com.sap.cds.ql.cqn.CqnStructuredTypeRef;
+import com.sap.cds.services.EventContext;
 import com.sap.cds.services.ServiceException;
 import com.sap.cds.services.draft.DraftService;
+import com.sap.cds.services.draft.Drafts;
 import com.sap.cds.services.messages.Message;
 import com.sap.cds.services.messages.Message.Severity;
 import com.sap.cds.services.persistence.PersistenceService;
 import com.sap.cds.services.runtime.CdsRuntime;
+import com.sap.cds.services.utils.DraftUtils;
 import java.util.Optional;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -41,8 +48,8 @@ class WithdrawTravelHandlerTest {
     @Test
     @WithMockUser("amy")
     void testOnWithdrawTravel_invalidEntryState() {
-        CqnSelect querySelect = Select.from(Travel_.class)
-            .where(t -> t.TravelUUID().eq("73757221A8E4645C17002DF03754AB66"))
+        CqnSelect querySelect = Select.from(Travel_.class,
+                f -> f.filter(t -> t.TravelUUID().eq("73757221A8E4645C17002DF03754AB66")))
             .columns(Travel_::TravelUUID, Travel_::TravelStatus_code);
         Travel travel = travelService.run(querySelect).single(Travel.class);
         assertEquals("X", travel.travelStatusCode());
@@ -56,8 +63,8 @@ class WithdrawTravelHandlerTest {
     @Test
     @WithMockUser("amy")
     void testOnWithdrawTravel_withdrawingStillPossible() {
-        CqnSelect querySelect = Select.from(Travel_.class)
-            .where(t -> t.TravelUUID().eq("76757221A8E4645C17002DF03754AB66"))
+        CqnSelect querySelect = Select.from(Travel_.class,
+                f -> f.filter(t -> t.TravelUUID().eq("76757221A8E4645C17002DF03754AB66")))
             .columns(Travel_::TravelUUID, Travel_::TravelStatus_code);
         Travel travel = travelService.run(querySelect).single(Travel.class);
         assertEquals("A", travel.travelStatusCode());
@@ -71,33 +78,60 @@ class WithdrawTravelHandlerTest {
     @Test
     @WithMockUser("amy")
     void testOnWithdrawTravel_afterWithdrawalPossible() {
-        CqnSelect querySelect = Select.from(Travel_.class)
-            .where(t -> t.TravelUUID().eq("67757221A8E4645C17002DF03754AB66"))
+        CqnSelect querySelect = Select.from(Travel_.class,
+                f -> f.filter(t -> t.TravelUUID().eq("67757221A8E4645C17002DF03754AB66")))
             .columns(Travel_::TravelUUID, Travel_::TravelStatus_code);
         Travel travel = travelService.run(querySelect).single(Travel.class);
         assertEquals("A", travel.travelStatusCode());
 
-        verifyError(() -> travelService.withdrawTravel(travel.ref()),
-            "Travel can only be withdrawn up to 24 hours before travel begins.");
+        var exception = assertThrows(ServiceException.class,
+            () -> travelService.withdrawTravel(travel.ref()));
+        assertEquals("Travel can only be withdrawn up to 24 hours before travel begins.",
+            exception.getLocalizedMessage());
+        Travel travelAfter = travelService.run(querySelect).single(Travel.class);
+        assertEquals("A", travelAfter.travelStatusCode());
     }
 
     @Test
     @WithMockUser("amy")
     void testOnWithdrawTravel_draft_withdrawingStillPossible() {
-        CqnSelect querySelect = Select.from(Travel_.class)
-            .where(t -> t.TravelUUID().eq("76757221A8E4645C17002DF03754AB66"))
-            .columns(Travel_::TravelUUID, Travel_::TravelStatus_code);
-        var travel = travelService.run(querySelect).single(Travel.class);
-        assertEquals("A", travel.travelStatusCode());
+        CqnSelect selectActive = Select.from(Travel_.class,
+            f -> f.filter(t -> t.TravelUUID().eq("75757221A8E4645C17002DF03754AB66").and(t.IsActiveEntity().eq(true))));
+        var travel = travelService.run(selectActive).single(Travel.class);
+        assertEquals("O", travel.travelStatusCode());
         DraftService draftService = (DraftService) travelService;
-        var draftTravelInsert = draftService.newDraft(Insert.into(Travel_.class).entry(travel)).single(Travel.class);
-//        CqnSelect selectDraft = Select.from(draftTravelInsert);
-//        Travel draftTravel = travelService.run(selectDraft).single(Travel.class);
+        draftService.newDraft(Insert.into(Travel_.class).entry(travel)).single(Travel.class).ref();
 
-        travelService.withdrawTravel(draftTravelInsert.ref());
+        Travel_ draftRef = CQL.entity(Travel_.class).filter(t -> t.TravelUUID().eq("75757221A8E4645C17002DF03754AB66").and(t.IsActiveEntity().eq(false)));
+        travelService.withdrawTravel(draftRef);
 
-        Travel travelAfter = travelService.run(querySelect).single(Travel.class);
-        assertEquals("W", travelAfter.travelStatusCode());
+
+        Travel activeTravelAfter = travelService.run(selectActive).single(Travel.class);
+        assertEquals("O", activeTravelAfter.travelStatusCode(), "active Travel should be unchanged");
+        Travel draftTravelAfter = travelService.run(Select.from(draftRef)).single(Travel.class);
+        assertEquals("W", draftTravelAfter.travelStatusCode(), "draft Travel should be changed");
+    }
+
+    public boolean isDraftEnabledAndActive(CqnStructuredTypeRef ref) {
+        return DraftUtils.isDraftEnabled(runtime.getCdsModel().findEntity(ref.targetSegment().id()).get())
+            && isActiveEntity(ref);
+    }
+
+    public boolean isActiveEntity(CqnStructuredTypeRef ref) {
+        Object isActiveEntity = CqnAnalyzer.create(runtime.getCdsModel()).analyze(ref).targetKeys()
+            .get(Drafts.IS_ACTIVE_ENTITY);
+        return isActiveEntity instanceof Boolean bool && bool;
+    }
+
+    public boolean isInactiveEntity(CqnStructuredTypeRef ref) {
+        Object isActiveEntity = CqnAnalyzer.create(runtime.getCdsModel()).analyze(ref).targetKeys()
+            .get(Drafts.IS_ACTIVE_ENTITY);
+        return (isActiveEntity instanceof Boolean bool && !bool.booleanValue());
+    }
+
+    public boolean isDraft(CqnStructuredTypeRef ref) {
+        return DraftUtils.isDraftTarget(ref, runtime.getCdsModel().findEntity(Travel_.CDS_NAME).get(),
+            runtime.getCdsModel());
     }
 
     void verifyError(Runnable action, String errorMessage) {
