af80d5c207b0e3387ba1c011dd47ad5db31cf829
Robin de Silva Jayasinghe <r.de.silva.jayasinghe@sap.com>
2025-06-16 16:17:36 +0200

    align WithdrawTravelHandler with documentation
    also adjust a bit of the test data in order to provoke
    the expected exception.

--- srv/src/main/java/com/sap/cap/sflight/processor/WithdrawTravelHandler.java
+++ srv/src/main/java/com/sap/cap/sflight/processor/WithdrawTravelHandler.java
@@ -1,27 +1,30 @@
 package com.sap.cap.sflight.processor;
 
-import cds.gen.sap.fe.cap.travel.TravelStatus;
 import cds.gen.sap.fe.cap.travel.TravelStatusCode;
 import cds.gen.travelservice.Travel;
 import cds.gen.travelservice.TravelService_;
 import cds.gen.travelservice.TravelWithdrawTravelContext;
 import cds.gen.travelservice.Travel_;
-import com.sap.cds.CdsData;
 import com.sap.cds.ql.Select;
 import com.sap.cds.ql.Update;
+import com.sap.cds.ql.cqn.AnalysisResult;
+import com.sap.cds.ql.cqn.CqnAnalyzer;
 import com.sap.cds.ql.cqn.CqnStructuredTypeRef;
 import com.sap.cds.services.ErrorStatuses;
 import com.sap.cds.services.ServiceException;
 import com.sap.cds.services.cds.ApplicationService;
 import com.sap.cds.services.draft.DraftService;
+import com.sap.cds.services.draft.Drafts;
 import com.sap.cds.services.handler.EventHandler;
 import com.sap.cds.services.handler.annotations.Before;
 import com.sap.cds.services.handler.annotations.On;
 import com.sap.cds.services.handler.annotations.ServiceName;
 import com.sap.cds.services.persistence.PersistenceService;
 import com.sap.cds.services.utils.DraftUtils;
-import java.time.LocalDate;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+import java.util.Map;
 
 @Component
 @ServiceName(TravelService_.CDS_NAME)
@@ -34,37 +37,36 @@ public class WithdrawTravelHandler implements EventHandler {
   }
 
   @Before(entity = Travel_.CDS_NAME)
-  public void check24HoursBeforeTravel(
-      final TravelWithdrawTravelContext context, CqnStructuredTypeRef travelRef) {
-    Travel travel =
-        ((ApplicationService) context.getService())
-            .run(Select.from(travelRef).columns(Travel_.BEGIN_DATE))
-            .first(Travel.class)
+  public void check24HoursBeforeTravel(final TravelWithdrawTravelContext context, CqnStructuredTypeRef travelRef) {
+    Travel travel = ((ApplicationService) context.getService()).run(
+                    Select.from(travelRef).columns(Travel_.BEGIN_DATE)).first(Travel.class)
             .orElseThrow(() -> new ServiceException(ErrorStatuses.BAD_REQUEST, "TRAVEL_NOT_FOUND"));
 
     if (travel.beginDate().isBefore(LocalDate.now().minusDays(1))) {
-      context
-          .getMessages()
-          .error("Travel can only be withdrawn up to 24 hours before travel begins.");
+      context.getMessages().error("Travel can only be withdrawn up to 24 hours before travel begins.");
     }
   }
 
   @On(entity = Travel_.CDS_NAME)
-  public void onWithdrawTravel(
-      final TravelWithdrawTravelContext context, CqnStructuredTypeRef travelRef) {
-    var isDraft =
-        DraftUtils.isDraftTarget(
+  public void onWithdrawTravel(final TravelWithdrawTravelContext context, CqnStructuredTypeRef travelRef) {
+    boolean isDraftTarget =DraftUtils.isDraftTarget(
             travelRef,
             context.getModel().findEntity(travelRef.targetSegment().id()).get(),
             context.getModel());
+    boolean isDraftEnabled = DraftUtils.isDraftEnabled(context.getTarget());
     var travel = Travel.create();
     travel.travelStatusCode(TravelStatusCode.WITHDRAWN);
-    var update = Update.entity(travelRef).data(travel);
-    if (isDraft) {
-      ((DraftService) context.getService()).patchDraft(update);
+    if (isDraftTarget) {
+      ((DraftService) context.getService()).patchDraft(Update.entity(travelRef).data(travel));
     } else {
-      persistenceService.run(update);
+      AnalysisResult analysis = CqnAnalyzer.create(context.getModel()).analyze(travelRef);
+      Map<String, Object> keys = analysis.targetKeyValues();
+      if (isDraftEnabled) {
+        keys.remove(Drafts.IS_ACTIVE_ENTITY);
+      }
+      persistenceService.run(Update.entity(context.getTarget()).matching(keys).data(travel));
     }
     context.setCompleted();
   }
+
 }
