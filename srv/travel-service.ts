8dd143598e44de3bee6d0ef001f045fd02069bf3
I548646 <paul.erlenwein@sap.com>
2025-10-14 17:16:08 +0200

    feat: align with xtravels implementation

--- srv/travel-service.ts
+++ srv/travel-service.ts
@@ -88,9 +88,10 @@ export class TravelService extends cds.ApplicationService { init() {
     const existingDraft = await SELECT.one(req.target.drafts.name).where(req.subject.ref[0].where)
       .columns(travel => { travel.DraftAdministrativeData.InProcessByUser.as('InProcessByUser') } )
     // @ts-ignore
-    if (!req.target.isDraft && existingDraft) req.error(423, `The travel is locked by ${existingDraft.InProcessByUser}.`);
-    // @ts-ignore
-    if (req.target.isDraft && existingDraft?.InProcessByUser !== req.user.id) req.error(423, `The travel is locked by ${existingDraft.InProcessByUser}.`);
+    // action called on active -> reject if draft exists
+    // action called on draft -> reject if not own draft 
+    if (!req.target.isDraft && existingDraft || req.target.isDraft && existingDraft?.InProcessByUser !== req.user.id)
+      req.error(423, `The travel is locked by ${existingDraft.InProcessByUser}.`);
   })
   // @ts-ignore
   this.on (acceptTravel, [Travel, Travel.drafts], req => UPDATE (req.subject) .with ({ TravelStatus_code: TravelStatusCode.Accepted }))
